7ef7a3bb700f175b57b497e983a809522ca1e865
Soma Sekhara <soma.sekhara@absolutelabs.co>
2025-11-21 13:04:29 +0530

    initial commit

---
+++ google-login-flask/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, redirect, url_for, session, render_template, request
+from google_auth_oauthlib.flow import Flow
+from google.oauth2 import id_token
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+import requests
+import os
+import pytz
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = 'YOUR_SUPER_SECRET_KEY_CHANGE_THIS'  # Change this!
+
+# Google OAuth Config
+SCOPES = [
+    'openid',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+]
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# Load credentials
+flow = Flow.from_client_secrets_file(
+    'credentials.json',
+    scopes=SCOPES,
+    redirect_uri='http://localhost:5000/callback'
+)
+
+def get_india_time():
+    ist = pytz.timezone('Asia/Kolkata')
+    now = datetime.now(ist)
+    return now.strftime("%d %B %Y, %I:%M:%S %p IST")
+
+@app.route('/')
+def index():
+    if 'google_user' in session:
+        user = session['google_user']
+        return render_template('home.html',
+                               name=user['name'],
+                               email=user['email'],
+                               picture=user.get('picture'),
+                               india_time=get_india_time())
+    
+    return '''
+    <h1 style="text-align:center; margin-top:100px; font-family:Arial">
+        Welcome! Please sign in
+    </h1>
+    <div style="text-align:center; margin-top:30px;">
+        <a href="/login">
+            <button style="padding:15px 30px; font-size:18px; background:#4285F4; color:white; border:none; border-radius:5px; cursor:pointer;">
+                <img src="https://developers.google.com/identity/images/g-logo.png" width="20" height="20" style="vertical-align:middle; margin-right:10px;">
+                Sign in with Google
+            </button>
+        </a>
+    </div>
+    '''
+
+@app.route('/login')
+def login():
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true',
+        prompt='select_account'
+    )
+    session['state'] = state
+    return redirect(authorization_url)
+
+@app.route('/callback')
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session.get('state') or session['state'] != request.args['state']:
+        return "State mismatch. Possible CSRF attack.", 400
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=os.getenv("GOOGLE_CLIENT_ID") or flow.client_config['client_id']
+    )
+
+    user_info = {
+        'name': id_info.get('name'),
+        'email': id_info.get('email'),
+        'picture': id_info.get('picture')
+    }
+
+    session['google_user'] = user_info
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
